85d821c96f0e02de13a6662cbaee781cd04342fa
Admin <Admin@DESKTOP-L0ALAT0>
2024-03-23 16:51:46 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoDeLaboratorios/DAL/DbContexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GestaodeLaboratorios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaodeLaboratorios.DAL
+{
+    public class DbContexto : DbContext
+    {
+        public DbContexto(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Computadores> Computadores { get; set; }
+    }
+
+}
---
+++ GestaoDeLaboratorios/Models/Computadores.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaodeLaboratorios.Models
+{
+    public class Computadores
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Processador { get; set; }
+        public string PlacaMae { get; set; }
+        public int Memoria { get; set; }
+        public int HD { get; set; }
+        public string NumeroPatrimonio { get; set; }
+        public DateTime DataCompra { get; set; }
+
+
+    }
+}
